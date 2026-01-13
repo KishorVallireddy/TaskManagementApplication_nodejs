cb39fde063e182e078c5fbdd627a887150c31fd7
KishorVallireddy <120686689+KishorVallireddy@users.noreply.github.com>
2026-01-13 11:48:05 +0530

    Implement base64 decoding for passwords in auth
    Added base64 decoding for password and confirmPassword during registration and login. Introduced isBase64 function to validate base64 strings.

--- task-manager-api/src/controllers/auth.controller.js
+++ task-manager-api/src/controllers/auth.controller.js
@@ -5,13 +5,13 @@ const User = require("../models/user.model");
 /* REGISTER */
 exports.register = async (req, res) => {
   let { username, password, confirmPassword } = req.body;
-
+ if (isBase64(password)){
  const decoded = Buffer.from(password, "base64").toString();
    password = decoded.split(":")[0];
 
     const decodedconfirmPassword = Buffer.from(confirmPassword, "base64").toString();
    confirmPassword = decodedconfirmPassword.split(":")[0];
-
+ }
   // 1. Validate input
   if (!username || !password || !confirmPassword) {
     return res.status(400).json({ message: "All fields are required" });
@@ -46,13 +46,18 @@ exports.register = async (req, res) => {
 /* LOGIN (already present) */
 exports.login = async (req, res) => {
   let { username, password } = req.body;
-
+console.log("password:"+username);
+console.log("before password:"+password);
   const user = await User.findOne({ username });
   if (!user) {
-    return res.status(401).json({ message: "Invalid credentials" });
+    return res.status(401).json({ message: "Invalid credentials 123" });
   }
+  if (isBase64(password)){
    const decoded = Buffer.from(password, "base64").toString();
    password = decoded.split(":")[0];
+  }
+   console.log("password:"+password);
+   console.log("user.passwordHash:"+user.passwordHash);
     const isMatch = await bcrypt.compare(password, user.passwordHash);
   if (!isMatch) {
     return res.status(401).json({ message: "Invalid credentials" });
@@ -76,4 +81,14 @@ exports.login = async (req, res) => {
       role: user.role
   });
 };
+function isBase64(str) {
+  try {
+    const decoded = Buffer.from(str, "base64").toString("utf-8");
+    const reEncoded = Buffer.from(decoded, "utf-8").toString("base64");
 
+    // normalize (remove padding differences)
+    return reEncoded.replace(/=+$/, "") === str.replace(/=+$/, "");
+  } catch {
+    return false;
+  }
+}
